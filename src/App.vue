4983a427efde83b39cf8f681a3a91b73347bea66
李晓庆 <lixiaoqing@tzt.cn>
2020-03-10 13:07:31 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <el-container style="height: 500px">
+      <el-header>header</el-header>
+      <el-container>
+        <el-aside width="200px">aside</el-aside>
+        <el-main>
+          <HelloWorld></HelloWorld>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.el-header{
+  background-color: green;
+}
+.el-aside{
+  background-color: blue;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <el-button type="primary" @click="visible = true">按钮</el-button>
+    <el-dialog :visible.sync="visible" title="hell-world"><p>HelloWorld111</p></el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
